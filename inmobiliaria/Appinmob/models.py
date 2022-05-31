83817e0bcc01cf03980d9719cd19af35651b2238
Mattioli Daniel <mattiolidaniel@gmail.com>
2022-05-30 22:31:57 -0300

    django + app + template + static + models - db(sin db)

---
+++ inmobiliaria/Appinmob/models.py
@@ -0,0 +1,33 @@
+from pyexpat import model
+from django.db import models
+from django.forms import BooleanField, CharField, EmailField, IntegerField
+
+# Create your models here.
+
+class Propiedad(models.Model):
+    titular = CharField(max_length=40)
+    ubicacion = CharField(max_length=40)
+    dimensiones = CharField(max_length=30)
+    poseecartel = BooleanField()
+
+
+class Martillero(models.Model):
+    nombre = CharField(max_length=40)
+    Apellido = CharField(max_length=40)
+    dni = IntegerField()
+
+class Ventas(models.Model):
+    nombremartillero = CharField(max_length=40)
+    comprador = CharField(max_length=40)
+
+class Alquileres(models.Model):
+    inquilino = CharField(max_length=40)
+    dueño = CharField(max_length=40)
+
+class Contacto(models.Model):
+    nombre = CharField(max_length=40)
+    apellido = CharField(max_length=40)
+    email = EmailField()
+    telefono = IntegerField()
+    mensaje = CharField(max_length=100)
+
